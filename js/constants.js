eed470357603fa93210950dbfae9168638b1d280
jamesliudotcc <james@jamesliu.cc>
2018-12-11 21:36:20 -0800

    Initial setup and consts no longer in game.js

--- js/constants.js
+++ js/constants.js
@@ -1,17 +1,15 @@
-const FOX = 'fox',
-  GOOSE = 'goose';
-
-const WIDTH = 7,
-  HEIGHT = 7;
-
-const NW = 0 - WIDTH - 1,
-  N = 0 - WIDTH,
-  NE = 0 - WIDTH + 1,
-  W = -1,
-  E = 1,
-  SW = WIDTH - 1,
-  S = WIDTH,
-  SE = WIDTH + 1;
+const FOX = 'fox', GOOSE = 'goose';
+const WIDTH = 7, HEIGHT = 7;
+const NW = 0 - WIDTH - 1, N = 0 - WIDTH, NE = 0 - WIDTH + 1, W = -1, E = 1, SW = WIDTH - 1, S = WIDTH, SE = WIDTH + 1;
 const directions = [N, NE, E, SE, S, SW, W, NW];
-
 const xmlns = 'http://www.w3.org/2000/svg';
+let startingState = {
+    foxWon: false,
+    geeseWon: false,
+    foxTurn: false,
+    foxJumped: false,
+    foxAt: 17,
+    geeseAt: [],
+    legalMoves: [],
+    legalJumps: [],
+};
