6a1e7c3a44267c23ee991491ca9156db7ffffecb
jamesliudotcc <james@jamesliu.cc>
2018-12-16 08:48:11 -0800

    dropshadow paints on drag-in and unpaints on drag-out

--- ts/update.ts
+++ ts/update.ts
@@ -60,30 +60,6 @@ function update(message: messageToUpdate, previousState: any): any {
     return fromJS(newState);
   }
 
-  if (message.tileMouseOver) {
-    const legalMoves = currentState
-      .get('legalMoves')
-      //@ts-ignore
-      .filter(a => a[0] === Number(message.tileMouseOver))
-      //@ts-ignore
-      .map(a => a[1]);
-
-    const legalJumps = currentState
-      .get('legalJumps')
-      //@ts-ignore
-      .filter(a => a[0] === Number(message.tileMouseOver))
-      //@ts-ignore
-      .map(a => a[1]);
-    newState.moveSuggest = legalMoves.concat(legalJumps);
-    return fromJS(newState);
-  }
-
-  if (message.tileMouseOut) {
-    console.log('Implement clearing pickup shadow');
-    newState.clearSuggestions = true;
-    return fromJS(newState);
-  }
-
   if (message.dropTargetOn) {
     if (message.clearDragShadow) {
       newState.dropTargetOff = message.dropTargetOn;
